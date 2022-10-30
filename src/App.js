21dbaf746305e36dcf39e6415386e659fa97bded
hichammayat <hichammayat07@gmail.com>
2022-10-30 21:31:23 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+class App extends React.Component {
+
+  state = { isProfileToggle:true }
+  Person = {
+    fullName: 'Hicham MAYAT',
+    bio: '',
+    imgSrc:  "logo192.png" ,
+    profession: 'Student at GoMyCode',
+    ToggleView : true
+  }
+  
+  // Funtionc Called onclick of Show Profile
+  ProfileToggleView(){
+    // Checks if isProfileToggle & ToggleView are true which means profile will be shown if true and clicked than set to false
+    if (this.state.isProfileToggle === true) {
+      this.setState({isProfileToggle : false})
+      this.Person.ToggleView = false
+      console.log(`Profile View Toggle Off!`)
+    } else {
+      // it will be false which means profile not shown and it will be set to true onclick
+      this.setState({isProfileToggle : true})
+      this.Person.ToggleView = true
+      console.log(`Profile View Toggle On!`)
+    }
+  }
+  render () {
+    return (
+      <div className="user-box-info" >
+        <img 
+          alt=''
+          className='profilePicture' 
+          src={ this.Person.imgSrc } 
+          style={  this.Person.ToggleView === false ? {contentVisibility: 'hidden'}: {contentVisibility: 'visible'}} 
+        />
+
+        <div 
+          className="text" 
+          style={ this.Person.ToggleView  === false ? {visibility: 'hidden'} : {contentVisibility: 'visible'}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <p className="fullName">Full Name : { this.Person.fullName }</p>
+          <p className="bio">Bio : { this.Person.bio }</p>
+          <p className="profession">Profession: { this.Person.profession }</p>
+        </div>
+        <div className="interactions-btn">
+          <button onClick={ ()=> this.ProfileToggleView()} className="btn-toggle-profile">Show Profile</button>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
